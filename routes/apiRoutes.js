673e93bff3f4bcb894f20d1698edc329d6531e60
lucasBRYG <adio.lucas@gmail.com>
2020-11-06 14:29:30 -0500

    fixing bug with delete endpoint

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,28 +11,35 @@ module.exports = function(app) {
     
     //POST Requests
     app.post("/api/notes", function(req, res) {
-        let id = data[data.length-1].id;
-        let newNote = req.body;
-        newNote["id"] = id + 1;
-        data.push(newNote);
 
+        let newNoteId;
+        let newNote = req.body;
+        if(data.length === 0) {
+            newNoteId = 1;
+        }else {
+            newNoteId = data[data.length-1]["id"] + 1;
+        }
+        newNote.id = newNoteId;
+        data.push(newNote);
         fs.writeFileSync("./db/db.json", JSON.stringify(data, null, 2));
         res.json(data);
+
     });
 
     //DELETE Requests
     app.delete("/api/notes/:id", function(req, res) {
 
-        const noteID = parseInt(req.params.id);
+        const id = parseInt(req.params.id);
 
-        for(let i = 0; i < data.length; i++) {
-            if(noteID = data[i].id) {
-                data.splice(i, 1);
-                fs.writeFileSync("./db/db.json", JSON.stringify(data, null, 2));
+        for (let i=0; i < data.length; i++){
+            if (id === data[i].id) { // Finding the note to be deleted.
+                data.splice(i,1);
+                let newContent = JSON.stringify(data,null,2); 
+                fs.writeFileSync("./db/db.json", newContent);
             }
-        };
-
+        }
         res.json(data);
+
     });
 
 };
